1f19b1fe126edbed04d90c67fefd00cbc6583bfa
Prasanth Vaaheeswaran <prasanthv@icloud.com>
2018-04-09 04:04:22 -0400

    Hook everything up

--- index.js
+++ index.js
@@ -5,38 +5,23 @@ async function main() {
   try {
     const stdin = await getStdin();
     const instructions = stdinParser(stdin);
-    return instructions;
+    let results = [];
+
+    instructions.query.forEach(q => {
+      results.push(instructions.store.getHome(q));
+    });
+
+    return results;
   } catch (e) {
     throw e;
   }
 }
 
 main()
-  .then(data => console.log(JSON.stringify(data)))
+  .then(result => {
+    result.forEach(home => {
+      console.log(`"${home.name}" "${home.region}" ${home.rating}`);
+    });
+  })
   .catch(e => console.log(e));
 
-function getHome(home, locationQuery) {
-  let _home = null;
-  let allHomes = [];
-  if ((locationQuery.level = 1)) {
-    let country = locationQuery.country;
-    Object.keys(db[country]).forEach(province => {
-      console.log(province);
-      Object.keys(db[country][province]).forEach(city => {
-        let homes = db[country][province][city];
-        console.log(homes);
-        for (let i = 0; i < homes.length; i++) {
-          if (homes[i].name == home) {
-            _home = homes[i];
-            return;
-          }
-        }
-      });
-    });
-  } else {
-    throw new Error('Unsupported depth', locationQuery.level);
-  }
-  console.log(_home);
-  console.log(allHomes);
-  return _home;
-}
